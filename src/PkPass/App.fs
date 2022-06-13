d969072426c1eed67303ebd6667c24a928407d52
Claas <claas.moehlmann@outlook.com>
2022-06-13 23:28:58 +0200

    Implement messy but working proof of concept loading
    Change service worker to be more maintainable

--- src/PkPass/App.fs
+++ src/PkPass/App.fs
@@ -24,13 +24,22 @@ open PkPass.PassKit.Package
 
 module Command = Cmd
 
+type PassDetailsPageModel = PassDetailsPageModel of Result<Pass, DeserializationError>
+
 type Page =
     | [<EndPoint "/">] Home
     | [<EndPoint "/open">] Open
     | [<EndPoint "/open/{fileName}">] OpenFile of fileName: string
+    | [<EndPoint "/pass/details">] ShowPass of PageModel<PassDetailsPageModel>
 
 type FileName = FileName of string
 
+
+type Error =
+    | LoadUrlsError of Exception
+    | LoadFromCacheError of Exception
+    | FileNameNotFound of fileName: string
+    | Unexpected of Exception
 
 type Model =
     { page: Page
@@ -38,7 +47,8 @@ type Model =
       background: PassBackground option
       logo: PassLogo option
       thumbnail: PassThumbnail option
-      passResult: Result<Pass, DeserializationError> option }
+      passResult: Result<Pass, DeserializationError> option
+      packageResult: Result<PassPackage, Error> option }
 
 let initializeModel () =
     { page = Home
@@ -46,15 +56,14 @@ let initializeModel () =
       background = None
       logo = None
       thumbnail = None
-      passResult = None }
+      passResult = None
+      packageResult = None }
 
-type Error =
-    | LoadUrlsError of Exception
-    | LoadFromCacheError of Exception
 
 type Message =
     | SetPage of Page
     | SetPassCacheUrls of string array
+    | SetLoadPassPackageResult of Result<PassPackage, Error>
     | SetPassResult of Result<Pass, DeserializationError> option
     | SetPassBackground of PassBackground option
     | SetPassLogo of PassLogo option
@@ -90,7 +99,7 @@ let update (jsRuntime: IJSRuntime) (logger: ILogger) (client: HttpClient) messag
         | OpenFile fileName ->
             // Find cache urls that end with file name
             // Load pass from cache
-            let getPackage (fileName : string) =
+            let getPass (fileName: string) =
                 model.cacheUrls
                 |> Array.tryFind (fun url -> url.EndsWith fileName)
                 |> Option.map (fun url ->
@@ -98,23 +107,65 @@ let update (jsRuntime: IJSRuntime) (logger: ILogger) (client: HttpClient) messag
                         try
                             // Load from cache
                             let! stream = client.GetStreamAsync url
-                            return stream |> AsStream |> Ok 
+                            return stream |> AsStream |> Ok
                         with
                         | exception' -> return LoadFromCacheError exception' |> Error
                     })
-            let command = Command.OfTask.perform getPackage  
-            model, Command.none
+                |> Option.defaultValue (
+                    fileName
+                    |> FileNameNotFound
+                    |> Error
+                    |> Task.FromResult
+                )
+
+            let handleError exception' =
+                Message.LogError (Error.Unexpected exception')
+            let command = Command.OfTask.either getPass fileName SetLoadPassPackageResult handleError
+            model, command
         | _ -> { model with page = page }, Command.none
+    | SetLoadPassPackageResult result ->
+        match result with
+        | Error error ->
+            { model with packageResult = Some result }, Command.none
+        | Ok package ->
+            // Set package and initialize loading pass
+            let loadPass package = package |> getPass |> Some
+            let handleError exception' =
+                Message.LogError (Error.Unexpected exception')
+            let command = Command.OfFunc.either loadPass package Message.SetPassResult handleError
+            { model with packageResult = Some result }, command
+    | SetPassResult passResultOption ->
+        
+        // Set result and switch to display page
+        let createPageModel model = { Model = model }
+
+        let command =
+            passResultOption
+            |> Option.map (fun result ->
+                Command.OfFunc.perform
+                    // Show pass page with result. Ye, nesting of these fun is no fun
+                    (fun () ->
+                        result
+                        |> PassDetailsPageModel
+                        |> createPageModel
+                        |> ShowPass)
+                    ()
+                    SetPage)
+            |> Option.defaultValue Command.none
+
+        { model with passResult = passResultOption }, command
     | SetPassCacheUrls urls -> { model with cacheUrls = urls }, Command.none
     | SetPassBackground background -> { model with background = background }, Command.none
     | SetPassLogo logo -> { model with logo = logo }, Command.none
     | SetPassThumbnail thumbnail -> { model with thumbnail = thumbnail }, Command.none
-    | SetPassResult passResultOption -> { model with passResult = passResultOption }, Command.none
     | LogError error ->
         match error with
-        | LoadUrlsError ``exception`` ->
-            logger.LogError(``exception``, "Error while loading files from cache urls")
-            model, Command.none
+        | LoadUrlsError ``exception`` -> logger.LogError(``exception``, "Error while loading urls")
+        | FileNameNotFound fileName -> logger.LogError("Could not find file with this name in cache {Name}", fileName)
+        | LoadFromCacheError ``exception`` -> logger.LogError(``exception``, "Error while loading files from cache")
+        | Unexpected ``exception`` -> logger.LogError(``exception``, "An unexpected error occurred")
+        model, Command.none
+
 
 let renderHeaderField field =
     div {
@@ -237,8 +288,7 @@ let barcode (passDefinition: PassDefinition) =
     | Some (Barcode (alternateText, format, message, messageEncoding)) ->
         match format with
         | Qr ->
-            let (Image.Base64 base64String) =
-                Barcode.createQrCode message
+            let (Image.Base64 base64String) = Barcode.createQrCode message
 
             let source = createPngDataUrl base64String
 
@@ -312,7 +362,25 @@ let homePage (model: Model) (dispatch: Message Dispatch) =
             ``class`` "p-4"
             h1 { "Passes" }
 
-            ul { forEach model.cacheUrls (fun url -> li { url }) }
+            ul {
+                forEach model.cacheUrls (fun url ->
+                    li {
+                        a {
+                            href url
+                            url
+                        }
+                    })
+
+                li {
+                    a {
+                        href "https://localhost:62042/open/pass.pkpass"
+                        "GO"
+                    }
+                    input {
+                        ``type`` "checkbox"
+                    }
+                }
+            }
 
             button {
                 on.click (fun _ -> ())
@@ -321,14 +389,42 @@ let homePage (model: Model) (dispatch: Message Dispatch) =
         }
     }
 
+let passDetails (PassDetailsPageModel pageModel) model =
+    match pageModel with
+    //TODO actual error card/info/something
+    | Error error -> div { "Oh no something went wrong and there should be a helpful error card here but it isn't. Sorry :L" }
+    | Ok pass ->
+        match pass with
+        | EventTicket (passDefinition, passStructure) -> eventTicket model passDefinition passStructure
+        | _ -> p { "Sorry this pass type is not yet supported :(" }
+        
 let view (model: Model) (dispatch: Message Dispatch) =
     match model.page with
     | Home -> homePage model dispatch
     | Open -> openPage model
     | OpenFile fileName -> openPage model
+    //TODO put all required properties like background in page model
+    | ShowPass { Model = pageModel } -> passDetails pageModel model
 
-let router =
-    Router.infer SetPage (fun model -> model.page)
+
+//let router = Router.infer SetPage (fun model -> model.page)
+let router: Router<Page, Model, Message> =
+    { getEndPoint = fun model -> model.page
+      setRoute =
+        fun path ->
+            match path.Trim('/').Split('/') with
+            | [||] -> Some Page.Home
+            | [| "open" |] -> Some Page.Open
+            | [| "open"; fileName |] ->
+                Some(Page.OpenFile(fileName))
+            | _ -> Some Page.Home
+            |> Option.map SetPage
+      getRoute =
+        function
+        | Home -> "/"
+        | Open -> "/open"
+        | OpenFile fileName -> $"/open/{fileName}"
+        | _ -> "/" }
 
 
 let getProperty<'T> (reference: IJSObjectReference) (properties: string array) (jsRuntime: IJSRuntime) =
@@ -341,7 +437,7 @@ let program (jsRuntime: IJSRuntime) logger (client: HttpClient) =
     let update = update jsRuntime logger client
 
 
-    let laodCacheUrls () =
+    let loadCacheUrls () =
         task {
             let! cache = jsRuntime |> CacheStorage.open' "files"
 
@@ -352,8 +448,7 @@ let program (jsRuntime: IJSRuntime) logger (client: HttpClient) =
                 match request with
                 | Request reference -> getProperty<{| url: string |}> reference [| "url" |] jsRuntime
 
-            let getUrlTasks =
-                requests |> Seq.map getRequestUrl
+            let getUrlTasks = requests |> Seq.map getRequestUrl
 
             let! urls = Task.WhenAll getUrlTasks
             return urls |> Array.map (fun request -> request.url)
@@ -361,8 +456,7 @@ let program (jsRuntime: IJSRuntime) logger (client: HttpClient) =
 
     let logError = LoadUrlsError >> LogError
 
-    let startCommand =
-        Command.OfTask.either laodCacheUrls () SetPassCacheUrls logError
+    let startCommand = Command.OfTask.either loadCacheUrls () SetPassCacheUrls logError
 
     Program.mkProgram (fun _ -> initializeModel (), startCommand) update view
     |> Program.withRouter router
@@ -385,5 +479,4 @@ type App() =
     member val HttpClient = Unchecked.defaultof<HttpClient> with get, set
 
 
-    override this.Program =
-        program this.JSRuntime this.Logger this.HttpClient
+    override this.Program = program this.JSRuntime this.Logger this.HttpClient
