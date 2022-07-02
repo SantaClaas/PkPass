ea000fecec1269e0cbf5196f5ccb9eb8439818c6
SantaClaas <claas.moehlmann@t-online.de>
2022-07-02 17:07:58 +0000

    Deploying to gh-pages from @ SantaClaas/PkPass@adbbc24b99cf4cdb23c40138eb5c80a41e5fa80e 🚀

--- service-worker.js
+++ service-worker.js
@@ -76,4 +76,4 @@ async function onFetch(event) {
     await cache.put(request, response);
 
     return Response.redirect(path, 303);
-}/* Manifest version: 4+ko+7Q1 */
+}/* Manifest version: 9hcLjk5A */
