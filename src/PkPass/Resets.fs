2f7cf8ccca27166ad67bfb1763f615ae95339b98
Claas <claas.moehlmann@outlook.com>
2022-12-10 18:57:35 +0100

    Add documentation

--- src/PkPass/Resets.fs
+++ src/PkPass/Resets.fs
@@ -1,4 +1,7 @@
-﻿module PkPass.Resets
+﻿/// <summary>
+/// Module to change names of types and functions to change naming to make them easier to understand
+/// </summary>
+module PkPass.Resets
 
 // This file is comparatively equal in function to CSS resets
 // I don't like abbreviations because they feel like a product of laziness, are harder to read and don't save time writing
