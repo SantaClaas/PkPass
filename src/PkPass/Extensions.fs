a945154c5369309075e2102d9622812bca42d2e6
Claas <claas.moehlmann@outlook.com>
2022-12-10 18:57:25 +0100

    Add extensions module to bundle extensions of external modules

---
+++ src/PkPass/Extensions.fs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// Commonly used useful extensions to the base library
+/// </summary>
+module PkPass.Extensions
+
+    module Option =
+        let fromTry = function
+            | true, value -> Some value
+            | false, _ -> None
